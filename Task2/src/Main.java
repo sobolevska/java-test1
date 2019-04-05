ac1cf8543b911fd7e262dcf5e165d2793abe934d
JS <jelena1984@gmail.com>
2019-04-05 20:45:19 +0300

    Task2 done

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -11,16 +11,26 @@ public class Main {
 
 
 
-        while  (fnumber = 100) {
+        if  (fnumber == 100) {
+            System.out.println("The room is full, we are sorry, but we cant take any more guests.");    }
+
+ else  {
+
+        System.out.println("How many people are in the q?");
+    }
+
+        Scanner sn= new Scanner(System.in);
+      int snumber = sn.nextInt();
+
+      int space= (fnumber+snumber);
+
+              if (space <=100) {
+                  System.out.println("Great, everione can fit in.");    }
+              else {
+                  System.out.println("Sorry, everyone can't get in?");
+              }
+}
 
 
 
-        if (fnumber < 100) {
-
-            System.out.println("How many people are in the queue?");
-
-
-            System.out.println("The room is full, we are sorry, but we cant take any more guests.");
-
-    }}
 }
